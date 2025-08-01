5d218d6f1dc55a4f1142bcc1a60f91c4cf961b58
Franco Montechiani <fnmontechiani@gmail.com>
2025-08-01 16:33:32 -0300

    Termine de hacer el componente que muestra las imagenes en la pagina de proyecto.

--- src/components/DisplayTag.jsx
+++ src/components/DisplayTag.jsx
@@ -2,7 +2,7 @@ export default function DisplayTag({ text, ubication }) {
   return (
     <>
       <span
-        className={`absolute scale-0 group-hover:scale-100 transition transform w-max bg-fuchsia-950 text-white text-xs 2xl:text-lg px-2 py-1 rounded shadow z-10 ${ubication}`}
+        className={`absolute scale-0 group-hover:scale-100 transition transform w-max bg-fuchsia-950 text-white text-xs 2xl:text-lg px-2 py-1 rounded-md shadow-md z-50 ${ubication}`}
       >
         {text}
       </span>
