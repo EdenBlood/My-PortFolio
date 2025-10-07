59a41c2845d128dbb4746cda2c1128b396efcee3
Franco Montechiani <fnmontechiani@gmail.com>
2025-10-07 15:45:38 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import preact from '@astrojs/preact'
 import path from 'node:path'
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [preact()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src')
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), 'src')
+      }
     }
   },
+  integrations: [preact()],
   image: {
     service: {
       provider: 'https://images.pexels.com/'
